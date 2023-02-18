97c25cd562b66a6b169bd59d7497d8d24d0a5baf
JoshAmpofo <joshampofo92@gmail.com>
2023-02-18 15:39:53 +0000

    Add temp function

--- sort.h
+++ sort.h
@@ -29,4 +29,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/* Temp functions */
+void swap(listint_t **head, listint_t *nd_1, listint_t *nd_2);
+
+
 #endif /* SORT_H */
